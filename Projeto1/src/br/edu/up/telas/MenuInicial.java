c44919efcf201af867a33875cf70900b3792cc3e
makzs <erikmakiak@gmail.com>
2024-04-15 09:43:20 -0300

    adicionandos projetos

---
+++ Projeto1/src/br/edu/up/telas/MenuInicial.java
@@ -0,0 +1,11 @@
+package br.edu.up.telas;
+
+public class MenuInicial {
+
+    public void mostrar() {
+        System.out.println("---------------------------------------------");
+        System.out.println("\tMenu Inicial");
+        System.out.println("---------------------------------------------");
+
+    }
+}
