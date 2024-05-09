f580401199eba49dc2f1d976e798f2be3adcb8ee
vts1978 <vinicioussouza@gmail.com>
2024-05-09 13:54:19 -0300

    Implementando Projeto2

--- Projeto2/src/App.java
+++ Projeto2/src/App.java
@@ -1,5 +1,17 @@
+import br.edu.up.Models.Ponto;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Ponto ponto1 = new Ponto(3, 4);
+        Ponto ponto2 = new Ponto(6, 8);
+
+        double distancia = ponto1.calcularDistancia(ponto2);
+        System.out.println("Distância entre os pontos: " + distancia);
+
+        double distanciaOrigem = ponto1.calcularDistanciaOrigem();
+        System.out.println("Distância até a origem: " + distanciaOrigem);
+
+        Ponto pontoMedio = ponto1.calcularPontoMedio(ponto2);
+        System.out.println("Ponto médio: (" + pontoMedio.getX() + ", " + pontoMedio.getY() + ")");
     }
 }
