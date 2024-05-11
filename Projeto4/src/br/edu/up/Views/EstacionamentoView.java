0a557d5f4625d6c123ec0f0095017aa41c526c67
vts1978 <vinicioussouza@gmail.com>
2024-05-11 11:29:26 -0300

    refact 04

--- Projeto4/src/br/edu/up/Views/EstacionamentoView.java
+++ Projeto4/src/br/edu/up/Views/EstacionamentoView.java
@@ -17,36 +17,17 @@ public class EstacionamentoView {
     public void exibirMenu() {
         boolean continuar = true;
         while (continuar) {
-            System.out.println("\nMenu:");
-            System.out.println("1. Entrada de carro");
-            System.out.println("2. Saída de carro");
-            System.out.println("3. Emitir relatório");
-            System.out.println("4. Sair");
-            System.out.print("Escolha uma opção: ");
-
-            int opcao;
-            opcao = scanner.nextInt();
-            scanner.nextLine();
+            exibirOpcoesMenu();
+            int opcao = lerOpcaoMenu();
             switch (opcao) {
                 case 1:
-                    var carro = lerDadosCarro();
-                    var addCarro = estacionamentoController.entradaCarro(carro);
-                    if(addCarro)
-                    {
-                        mostrarMensagem("Carro adicionado com sucesso");
-                    }
-                    else
-                    {
-                        mostrarMensagem("Limite atingido");
-                    }
+                    entradaCarro();
                     break;
                 case 2:
-                    var placa = lerPlaca();
-                    estacionamentoController.saidaCarro(placa);
-                    mostrarMensagem("Carro removido com sucesso");
+                    saidaCarro();
                     break;
                 case 3:
-                    mostrarMensagem(estacionamentoController.emitirRelatorio());
+                    emitirRelatorio();
                     break;
                 case 4:
                     continuar = false;
@@ -57,33 +38,61 @@ public class EstacionamentoView {
         }
     }
 
-    public String lerPlaca() {
-        System.out.print("Digite a placa do carro: ");
-        return scanner.nextLine();
+    private void exibirOpcoesMenu() {
+        System.out.println("\nMenu:");
+        System.out.println("1. Entrada de carro");
+        System.out.println("2. Saída de carro");
+        System.out.println("3. Emitir relatório");
+        System.out.println("4. Sair");
+        System.out.print("Escolha uma opção: ");
     }
 
-    public Carro lerDadosCarro() {
-        System.out.print("Digite o modelo do carro: ");
-        String modelo = scanner.nextLine();
+    private int lerOpcaoMenu() {
+        return scanner.nextInt();
+    }
+
+    private void entradaCarro() {
+        var carro = lerDadosCarro();
+        var addCarro = estacionamentoController.entradaCarro(carro);
+        if (addCarro) {
+            mostrarMensagem("Carro adicionado com sucesso");
+        } else {
+            mostrarMensagem("Limite atingido");
+        }
+    }
+
+    private void saidaCarro() {
+        var placa = lerPlaca();
+        var carroParaRemover = estacionamentoController.saidaCarro(placa);
+        if (carroParaRemover == null) {
+            mostrarMensagem("Carro não encontrado");
+        } else {
+            mostrarMensagem("Carro removido com sucesso");
+        }
+    }
+
+    private void emitirRelatorio() {
+        mostrarMensagem(estacionamentoController.emitirRelatorio());
+    }
+
+    private String lerPlaca() {
         System.out.print("Digite a placa do carro: ");
-        String placa = scanner.nextLine();
+        return scanner.next();
+    }
+
+    private Carro lerDadosCarro() {
+        System.out.print("Digite o modelo do carro: ");
+        String modelo = scanner.next();
+        System.out.print("Digite a placa do carro: ");
+        String placa = scanner.next();
         System.out.print("Digite a cor do carro: ");
-        String cor = scanner.nextLine();
+        String cor = scanner.next();
         System.out.println("----------------------------------------");
 
         return new Carro(modelo, placa, cor);
     }
 
-    public void mostrarMensagem(String mensagem) {
+    private void mostrarMensagem(String mensagem) {
         System.out.println(mensagem);
-    }
-
-    public void mostrarMensagemErro(String mensagem)
-    {
-        System.err.println(mensagem);
-    }
-
-    public Scanner getScanner() {
-        return scanner;
     }
 }
